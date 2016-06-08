59e29886a79a0fc49b4ad758addd3d65c34c84f1
huguojun <www.guojunkuaile@qq.com>
2016-06-08 17:59:12 +0800

    修改发现页面

--- app/src/main/java/com/iwind/tj_tax/Find/Act_Know.java
+++ app/src/main/java/com/iwind/tj_tax/Find/Act_Know.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.easemob.easeui.ui.EaseBaseActivity;
+import com.easemob.easeui.widget.EaseTitleBar;
 import com.iwind.tj_tax.R;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * 办税须知
@@ -13,12 +16,17 @@ import com.iwind.tj_tax.R;
  * 邮箱：www.guojunkuaile@qq.com
  */
 public class Act_Know extends EaseBaseActivity {
-
+    @ViewInject(R.id.title_bar)
+    EaseTitleBar title_bar;
 
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         setContentView(R.layout.activity_act__know);
+        ViewUtils.inject(this);
+        InitView();
+        InitData();
+        setOnClickListener();
     }
 
     @Override
@@ -28,7 +36,8 @@ public class Act_Know extends EaseBaseActivity {
 
     @Override
     public void InitView() {
-
+        title_bar.setLeftImageResource(R.drawable.ease_mm_title_back);
+        title_bar.setTitle(getResources().getString(R.string.must_know));
     }
 
     @Override
