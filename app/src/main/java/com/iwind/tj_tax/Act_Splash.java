6b67613eb4c9d17d5cc28dfbe03b02343da8b79d
huguojun <www.guojunkuaile@qq.com>
2016-06-16 17:30:51 +0800

    欢迎页面添加动画

--- app/src/main/java/com/iwind/tj_tax/Act_Splash.java
+++ app/src/main/java/com/iwind/tj_tax/Act_Splash.java
@@ -1,13 +1,20 @@
 package com.iwind.tj_tax;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.easemob.easeui.ui.EaseBaseActivity;
 import com.iwind.App.MyApplication;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * 作者：HuGuoJun
@@ -16,27 +23,57 @@ import com.iwind.App.MyApplication;
  */
 public class Act_Splash extends EaseBaseActivity {
 
+    @ViewInject(R.id.iv_splash)
+    ImageView iv_splash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act__splash);
+        ViewUtils.inject(this);
         InitView();
     }
 
     @Override
     public void InitView() {
-        new Handler().postDelayed(new Runnable() {
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(iv_splash, "scaleX", 1f, 1.13F);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(iv_splash, "scaleY", 1f, 1.13F);
+
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(2000).play(animatorX).with(animatorY);
+        set.start();
+        set.addListener(new Animator.AnimatorListener() {
             @Override
-            public void run() {
-                if (!MyApplication.GetUserName().equals("")) {
-                    startActivity(new Intent(context, MainActivity.class));
-                    finish();
-                } else {
-                    startActivity(new Intent(context, Act_Login.class));
-                    finish();
-                }
+            public void onAnimationStart(Animator animation) {
+
             }
-        }, 3000);
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!MyApplication.GetUserName().equals("")) {
+                            startActivity(new Intent(context, MainActivity.class));
+                            finish();
+                        } else {
+                            startActivity(new Intent(context, Act_Login.class));
+                            finish();
+                        }
+                    }
+                }, 3000);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 
     @Override
